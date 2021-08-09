7b46658f23de443b7588171a90956b610a5a45b8
Armen <armenzg@sentry.io>
2021-08-09 15:52:21 -0400

    Add Sentry SDK & always return success

--- 2021/main.py
+++ 2021/main.py
@@ -1,30 +1,40 @@
+import sentry_sdk
 from flask import jsonify, request, Flask
+
+# sentry_sdk.init(
+#     "https://060c8c7a20ae472c8b32858cb41c36a7@o19635.ingest.sentry.io/5899451",
+#     traces_sample_rate=1.0,
+# )
 
 app = Flask(__name__)
 
 
-class WorkflowState:
-    # data as per GH webhook's payload
-    def __init__(self, data) -> None:
-        # queued, started, completed
-        self.state = data["action"]
-        wf_job = data["workflow_job"]
-        # Does this id persist for the same wf after each commit?
-        self.id = wf_job["id"]  # e.g. 3283437728
-        # This, I assume is when a workflow is re-run
-        # Metadata about the run: https://api.github.com/repos/armenzg/sentry-hackweek/actions/runs/1113865931
-        self.run_id = wf_job["id"]  # e.g. 1113865931
-        self.startime_start = wf_job["started_at"]  # e.g. "2021-08-09T18:12:37Z"
-        self.startime_start = wf_job["name"]  # e.g. yml file -> "name": "salutation"
+def process_data(data):
+    steps = data["workflow_job"]["steps"]
+    for s in steps:
+        with sentry_sdk.start_transaction(op="task", name=s["name"]):
+            # with sentry_sdk.start_span(op="http", description="GET /") as span:
+            #     response = my_custom_http_library.request("GET", "/")
+            #     span.set_tag("http.status_code", response.status_code)
+            #     span.set_data("http.foobarsessionid", get_foobar_sessionid())
+            pass
+    return {"reason": "WIP"}, 200
 
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    if request.headers.get("X-GitHub-Event") != "workflow_job":
+def handle_event(data, headers):
+    return {}, 200
+    if headers.get("X-GitHub-Event") != "workflow_job":
         # We return 200 to make webhook not turn red
-        return jsonify({"reason": "Event not supported"}), 200
-    data = WorkflowState(request.data)
+        return {"reason": "Event not supported."}, 200
+
+    if data["action"] != "completed":
+        return ({"reason": "We cannot do anything with this workflow state."}, 200)
+
+    return process_data(data)
+
+
+@app.route("/", methods=["POST"])
+def main():
     print(request.data)
-    print(request.headers)
-    # return "<p>Hello, World!</p>"
-    return jsonify({}), 200
+    payload, http_code = handle_event(request.data, request.headers)
+    return jsonify(payload), http_code
