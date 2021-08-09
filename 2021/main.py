2238b40c4b398874db76239c1d60325af9037563
Armen Zambrano G <armenzg@sentry.io>
2021-08-09 14:12:22 -0400

    Trigger workflow

--- 2021/main.py
+++ 2021/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import jsonify, request, Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    print(request.data)
+    print(request.headers)
+    # return "<p>Hello, World!</p>"
+    return jsonify({}), 200
